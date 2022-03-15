8c07de09c7993ebe8d09f500e93d020b6db6dd92
jumbamark <jumbamark@yahoo.com>
2022-03-15 06:59:43 -0700

    Modified file according to Betty-coding style

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - computes and prints the sum of all the multiples of 3 and 5 below 1024
+ * main - computes and prints the sum of all the multiples
+ * of 3 and 5 below 1024
  * Return: 0 if succesfully executed
  */
 
@@ -11,9 +12,10 @@ int main(void)
 
 	for (a = 1; a < 1024; a++)
 	{
-			if ((a % 3) == 0 || (a % 5) == 0)
-				b += a;
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
 	}
+
 	printf("%d\n", b);
 	return (0);
 }
