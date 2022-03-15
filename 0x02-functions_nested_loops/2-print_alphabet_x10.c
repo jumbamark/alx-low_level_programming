88605ed998a7619f445820c2e6e36353f362d4b6
jumbamark <jumbamark@yahoo.com>
2022-03-15 03:07:13 -0700

    File 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase followed by a new line
+ * 
+ */
+
+void print_alphabet_x10(void)
+{
+	char alph;
+	int i = 0;
+
+	while (i < 10)
+	{
+		alph = 'a';
+
+		while (alph <= 'z')
+		{
+			_putchar(alph);
+			alph++;
+		}
+
+		putchar('\n');
+		i++;
+	}
+}
