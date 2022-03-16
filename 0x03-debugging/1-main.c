701050e4e917e335cacde3df716c377841b55e30
jumbamark <jumbamark@yahoo.com>
2022-03-16 00:32:53 -0700

    Fixed infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - causes an infite loop
+ * * main - causes an infinite loop
  * Return: 0
  */
 
@@ -9,16 +9,16 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");
+	printf("Infinte loop incoming :(\n");
 
 	i = 0;
 
-	/* while (i < 10) */
-	/*{*/
-		/*putchar(i);*/
-	/*}*/
+	/*while (i < 10)*/
+	{
+		putchar(i);
+	}
 
-	printf("Infinite loop avoided! \\0/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
