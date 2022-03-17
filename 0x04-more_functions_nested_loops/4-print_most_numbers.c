7cf988ace17d83467bf71fa37f1c823dd008b3e1
jumbamark <jumbamark@yahoo.com>
2022-03-17 03:58:59 -0700

    modified file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,7 +11,7 @@ void print_most_numbers(void)
 
 	for {; x < 10; x++)
 	{
-		if ((x == 2 + '0') || (x == 4 + '0'))
+		if ((x + '0' == 2) && (x + '0' == 4))
 			continue;
 		else
 			_putchar(x + '0');
