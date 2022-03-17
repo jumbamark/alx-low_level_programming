a912055a3f85ecdb30845f56c1fbd31a18332795
jumbamark <jumbamark@yahoo.com>
2022-03-17 02:45:16 -0700

    file 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9
+ * followed by a new line
+ */
+
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x + '0' == 2 || 4)
+			continue;
+		else
+			_putchar(x + '0');
+	}
+}
