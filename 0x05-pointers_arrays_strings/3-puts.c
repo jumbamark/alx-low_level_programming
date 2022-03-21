228ac600d26127ca21968646dbe83765b7da811b
jumbamark <jumbamark@yahoo.com>
2022-03-21 08:28:41 -0700

    file 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * to stdout
+ *
+ * @str - pointer to the string to be printed
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
