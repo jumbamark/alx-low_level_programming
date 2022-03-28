63fbd284b0fb0828157d43c98175f02231e567cc
jumbamark <jumbamark@yahoo.com>
2022-03-28 02:59:13 -0700

    file 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters
+ * to match in s
+ *
+ * Return: number of accepted bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, bool;
+
+	for (i = 0; *(s + i) != '\0'; j++)
+	{
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
+	}
+	return (i);
+}
