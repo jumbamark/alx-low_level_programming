8210be36c2d149427c29a0b19a8b9d938a10ad02
jumbamark <jumbamark@yahoo.com>
2022-03-28 02:46:06 -0700

    modified file 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,7 +13,7 @@ char *_strchr(char *s, char c)
 {
 	unsigned int a = 0;
 
-	for (; *(s + a) != '\0'; i++)
+	for (; *(s + a) != '\0'; a++)
 		if (*(s + a) == c)
 			return (s + a);
 	if (*(s + a) == c)
