a7dbe51ec7f73e13792f2202d544dc5661919c6e
jumbamark <jumbamark@yahoo.com>
2022-04-01 03:52:50 -0700

    file 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc:
+ * @argv:
+ *
+ * Return: 0 - success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
