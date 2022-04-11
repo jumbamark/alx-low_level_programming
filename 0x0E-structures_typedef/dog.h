e74620bd316de8a7a80205a194151c36a51009db
jumbamark <jumbamark@yahoo.com>
2022-04-11 13:32:28 -0700

    Including the free_dog function in header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,6 @@
 #define DOG_H
 
 /**
- * dog_t - typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
-/**
  * struct dog - struct that stores some information of a dog
  * @name: name of the dog
  * @age: age of the dog
@@ -24,9 +18,12 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 #endif
-
-
