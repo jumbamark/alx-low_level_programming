91693099915785880f4373a6d2fe6157127c4650
jumbamark <jumbamark@yahoo.com>
2022-04-11 10:54:23 -0700

    Added condition if pointer to struct is NULL

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,9 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initializes a variable of type struct dog
- * @d: struct dog
+ * @d: pointer to struct dog to initialize
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
@@ -12,6 +13,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
