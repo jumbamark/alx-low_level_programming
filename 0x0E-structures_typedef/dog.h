e33563d6a37f1dc33ae9fb9ea39cef2f7bca5737
jumbamark <jumbamark@yahoo.com>
2022-04-11 09:40:00 -0700

    Added a header file that contains dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name
+ * it's age and the name of the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
+
