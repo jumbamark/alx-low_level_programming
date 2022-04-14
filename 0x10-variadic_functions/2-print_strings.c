3b2fc6889784c6e27b9785b7cc6ad4ff155dc6e2
jumbamark <jumbamark@yahoo.com>
2022-04-14 06:52:03 -0700

    Modified file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,10 +1,10 @@
 #include "variadic_functions.h"
 
 /**
- * prints_strings - prints strings
- * @separator: separator bewteen strings
+ * print_strings - prints strings
+ * @separator: separator between strings
  * @n: number of arguments
- */ 
+ */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
