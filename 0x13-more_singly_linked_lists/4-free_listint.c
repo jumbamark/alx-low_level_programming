6c75858378b7906931af51bfb4b48180fac6faae
jumbamark <jumbamark@yahoo.com>
2022-04-25 10:30:28 -0700

    frees a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t _t list to be freed
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
