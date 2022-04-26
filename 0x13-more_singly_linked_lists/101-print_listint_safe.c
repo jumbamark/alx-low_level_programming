c7674a668d076017f0a8a9263b118e9fcae82d34
jumbamark <jumbamark@yahoo.com>
2022-04-26 02:33:25 -0700

    prints a linked list safely

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,7 +6,6 @@
  *
  * Return: no return.
  */
-
 void free_listp(listp_t **head)
 {
 	listp_t *temp;
@@ -25,15 +24,14 @@ void free_listp(listp_t **head)
 }
 
 /**
- * print_listint_safe - prints a linked list
- * @head: list of type listint_t to print
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
  *
- * Return: number of nodes in the list
+ * Return: number of nodes in the list.
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
+	size_t nnodes = 0;
 	listp_t *hptr, *new, *add;
 
 	hptr = NULL;
@@ -57,15 +55,15 @@ size_t print_listint_safe(const listint_t *head)
 			{
 				printf("-> [%p] %d\n", (void *)head, head->n);
 				free_listp(&hptr);
-				return (nodes);
+				return (nnodes);
 			}
 		}
 
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
-		nodes++;
+		nnodes++;
 	}
 
 	free_listp(&hptr);
-	return (nodes);
+	return (nnodes);
 }
