a8b301b5a80da5ec482863f59f14dcd6d42ed535
jumbamark <jumbamark@yahoo.com>
2022-04-26 02:06:18 -0700

    deletes a node at a given index in a linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
-	listint *current = NULL;
+	listint_t *current = NULL;
 	unsigned int i = 0;
 
 	if (*head == NULL)
