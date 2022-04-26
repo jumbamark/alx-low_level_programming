d60896edfce1c2af06f71849b66b95c7f5cbdb93
jumbamark <jumbamark@yahoo.com>
2022-04-26 02:32:17 -0700

    prints a linked list safely

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,30 @@
 #include "lists.h"
 
 /**
+ * free_listp - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
  * print_listint_safe - prints a linked list
  * @head: list of type listint_t to print
  *
