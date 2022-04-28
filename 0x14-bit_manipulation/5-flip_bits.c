b029a60ee6e2b5081cdbee885378c42b95c19ff8
jumbamark <jumbamark@yahoo.com>
2022-04-28 07:38:15 -0700

    Modified variable name

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count 0;
-	unsigned long int curret;
+	unsigned long int current;
 	unsigned long int exclusive = n ^ m;
 
 	for (i = 63; i >= 0; i--)
