92351b286290cdec4426622c0c3f8b47795da0c4
jumbamark <jumbamark@yahoo.com>
2022-07-01 16:04:00 +0000

    hash function implementing the djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,4 +1,4 @@
-include "hash_tables.h"
+#include "hash_tables.h"
 
 /**
  * hash_djb2 - function that generates a random number through
@@ -16,7 +16,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	hash = 5381;
 	while ((c = *str++))
 	{
-		hash = ((hash << 5)  + hash) + c; /* hash * 33 + c */
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
 	return (hash);
 }
